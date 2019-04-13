e3ace46188b9b54d1616a9ad7a636e2866373332
Zach <zachChilders@users.noreply.github.com>
2019-04-12 22:35:43 -0700

    added setstate

--- R/deanonymize.r
+++ R/deanonymize.r
@@ -6,22 +6,36 @@
 #' @examples
 #' getTable(table_name)
 getTable <- function(table_name) {
-    connstring <- Sys.getenv('localingressstring')
-    if (nchar(connstring) > 0) { # Detect local dev
-      library('RPostgreSQL')
-      conn <- connectToPostgres(connstring)
-      tables <- dbGetQuery(conn, paste("SELECT * FROM ", table_name))
-      tables
-    }
-    else { # We're in spark
-      library('SparkR')
-      s <- read.df(source="json", path='/FileStore/tables/secrets.json')
-      conn <- as.data.frame(s)
-      connstring <- conn$ingressconnectionstring[1]
-      df <- as.data.frame(read.jdbc(connstring, table_name))
-      df
-    }
+  connstring <- Sys.getenv('localingressstring')
+  if (nchar(connstring) > 0) { # Detect local dev
+    library('RPostgreSQL')
+    conn <- connectToPostgres(connstring)
+    tables <- dbGetQuery(conn, paste("SELECT * FROM ", table_name))
+    tables
+  }
+  else { # We're in spark
+    library('SparkR')
+    s <- read.df(source="json", path='/FileStore/tables/secrets.json')
+    conn <- as.data.frame(s)
+    connstring <- conn$ingressconnectionstring[1]
+    df <- as.data.frame(read.jdbc(connstring, table_name))
+    df
+  }
 }
+
+#' SetTable
+#'
+#' Sets a row state in the metadata table
+setTable <- function(table_row, state) {
+  connstring <- Sys.getenv('localingressstring')
+  if (nchar(connstring) > 0) { # Detect local dev
+    library('RPostgreSQL')
+    conn <- connectToPostgres(connstring)
+    query <- paste("UPDATE tables_index SET state =", state, "WHERE id =", table_row$id)
+    dbSendQuery(conn, query)
+      
+}
+
 
 #' ConnectToPostgres
 #'
@@ -30,11 +44,12 @@ getTable <- function(table_name) {
 #' @examples
 #' connectToPostgres(connstring)
 connectToPostgres <- function(connstring) {
-    connectionParams <- strsplit(connstring, "[|]")
-    pg <- dbDriver("PostgreSQL")
-    connection <- dbConnect(pg, connectionParams[[1]][1], user=connectionParams[[1]][2], password=connectionParams[[1]][3], dbname=connectionParams[[1]][4], port=5432)
-    connection
+  connectionParams <- strsplit(connstring, "[|]")
+  pg <- dbDriver("PostgreSQL")
+  connection <- dbConnect(pg, connectionParams[[1]][1], user=connectionParams[[1]][2], password=connectionParams[[1]][3], dbname=connectionParams[[1]][4], port=5432)
+  connection
 }
+
 
 
 #' Enum
